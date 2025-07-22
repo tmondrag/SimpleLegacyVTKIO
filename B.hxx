03d14d774e862fdbf57a4b31760d5eb37280115b
Tomas A. Mondragon <Tomas.A.Mondragon@erdc.dren.mil>
2025-07-21 19:49:27 -0500

    Expand on the Templated class example by adding a templated member function

--- B.hxx
+++ B.hxx
@@ -1,22 +1,40 @@
 // B.hxx - an example template class
 // The declaration and definition both have to be in the same file
-template <class t>
+
+template <typename t>
 class b
 {
 public:
 	b(t init);
 	~b();
+	template <typename k>
+	t add(k sumand);
+	t get();
 private:
 	t data;
 };
 
 // Implementation
-template <class t>
+template <typename t>
 b<t>::b(t init)
 {
 	this->data = init;
 }
-template <class t>
+template <typename t>
 b<t>::~b()
 {
+}
+
+template<typename t>
+inline t b<t>::get()
+{
+	return this->data;
+}
+
+template<typename t>
+template<typename k>
+t b<t>::add(k sumand)
+{
+	this->data = this->data + (t)sumand;
+	return this->data;
 }
--- main.cxx
+++ main.cxx
@@ -1,7 +1,14 @@
 //main.cxx
+#include <iostream>
 #include "B.hxx"
 void main()
 {
 	b<int> bi = b<int>(5);
 	b<float> bf = b<float>(float(5.1));
+
+	bi.add(9.99);
+	bf.add(9.99);
+
+	std::cout << bi.get() << std::endl;
+	std::cout << bf.get() << std::endl;
 }
